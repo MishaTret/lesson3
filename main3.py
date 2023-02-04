6a39c6e9211f2c87c1c9dd1f753e3b8504d1d28c
macbook <mishatret2010@gmail.com>
2023-02-04 14:57:02 +0200

    v2

--- main3.py
+++ main3.py
@@ -8,32 +8,107 @@ class Human:
         self.gladness = 50
         self.satiety = 50
         self.money = 100
+
     def get_home(self):
-        pass
+        self.home = House()
 
     def get_car(self):
-        pass
+        self.car = Auto(brands_of_car)
 
     def get_job(self):
-        pass
+        if self.car.drive():
+            pass
+        else:
+            self.to_repair()
+            return
+        self.job = Job(job_list)
 
     def eat(self):
-        pass
+        if self.home.food <= 0:
+            self.shopping("food")
+        else:
+            if self.satiety > 100:
+                self.satiety = 100
+                return
+            self.satiety += 5
+            self.satiety -= 5
 
     def work(self):
-        pass
+        if self.car.drive():
+            pass
+        else:
+            if self.car.fuel < 5:
+                self.shopping('fuel')
+                return
+            else:
+                self.to_repair()
+                return
+        self.money += self.job.salary
+        self.gladness -= self.job.gladness_less
+        self.satiety -= 4
 
-    def shopping(self):
-        pass
+    def shopping(self, manage):
+        if self.car.fuel < 5:
+            manage = 'fuel'
+        else:
+            self.to_repair()
+            return
+        if manage == 'fuel':
+            print('I bought fuel')
+            self.money -= 100
+            self.car.fuel += 100
+        elif manage == 'food':
+            print('Bought food')
+            self.money -= 50
+            self.home.food += 50
+        elif manage == 'delicacies':
+            print('Happy')
+            self.gladness += 10
+            self.satiety += 2
+            self.money -= 15
 
     def chill(self):
-        pass
+        self.gladness += 10
+        self.home.mess += 5
 
     def clean_home(self):
-        pass
+        self.gladness -= 5
+        self.home.mess = 0
 
     def to_repair(self):
+        self.car.strength += 100
+        self.money -= 50
+
+    def days_indexes(self, day):
+        day = f"Today is the {day} of {self.name}'s life"
+        print(f"{day:^50}", '\n')
+        human_indexess = self.name + " 's indexess"
+        print(f"{human_indexess:=^50}", '\n')
+        print(f'Money - {self.money}')
+        print(f'Satiety - {self.satiety}')
+        print(f'Gladness - {self.gladness}')
+        home_indexess = "Home indexess"
+        print(f"{home_indexess:^50}", '\n')
+        print(f'Food - {self.home.food}')
+        print(f'Mess - {self.home.mess}')
+
+    def is_alive(self):
         pass
+
+    def live(self, day):
+        if self.is_alive() == False:
+            return False
+        if self.home is None:
+            print('Settled in the house')
+            self.get_home()
+
+        dice = random.randint(1, 4)
+        if self.satiety < 10:
+            print("I'll go eat")
+            self.eat()
+        elif self.gladness < 20:
+            if self.home.mess < 15:
+                pass
 
 
 class Auto:
@@ -42,6 +117,31 @@ class Auto:
         self.fuel = brand_list[self.brand]['fuel']
         self.strength = brand_list[self.brand]['strength']
         self.consumption = brand_list[self.brand]['consumption']
+
+    def drive(self):
+        if self.strength > 0 and self.fuel > self.consumption:
+            self.fuel -= self.consumption
+            self.strength -= 1
+            return True
+        else:
+            print('The car is out of fuel.')
+            return False
+class House:
+    def __init__(self):
+        self.mess = 0
+        self.food = 0
+
+class Job:
+    def __init__(self, job_list):
+        self.job = random.choice(list(job_list))
+        self.salary = job_list[self.job]['salary']
+        self.gladness_less = job_list[self.job]['gladness_less']
+
+job_list = {
+    "Java developer": {"salary": 50, "gladness_less": 10},
+    "Python developer": {"salary": 40, "gladness_less": 3},
+    "C++ developer": {"salary": 45, "gladness_less": 25},
+    "Rust developer": {"salary":70, "gladness_less": 1 }}
 
 brands_of_car = {
     "BMW":{"fuel":100, "strength":100, "consumption": 6},
